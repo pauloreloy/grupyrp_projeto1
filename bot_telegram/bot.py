8d67ae505d9ff00fe170db58a66a07ad979a6f1f
Paulo Rogerio Eloy <pauloreloy@gmail.com>
2023-07-06 01:16:07 -0300

    teste

--- bot_telegram/bot.py
+++ bot_telegram/bot.py
@@ -8,13 +8,13 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['analisar', 'Analisar'])
 def analisar(message):
-    ans = "Por gentileza, envie a foto que deseja analisar"
+    ans = "Por gentileza, envie a foto que deseja analisar..."
     sent_msg = bot.send_message(message.chat.id, ans, parse_mode="Markdown")
     bot.register_next_step_handler(message, analisarFoto)
 
 @bot.message_handler(content_types=['photo'])
 def analisarFoto(message):
-    bot.send_message(message.chat.id, "Aguarde... foto em análise")
+    bot.send_message(message.chat.id, "Aguarde. Foto em análise...")
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
