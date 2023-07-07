93bd2ca608d6fef3f9d2907b64554c4a38ab890f
Paulo Rogerio Eloy <pauloreloy@gmail.com>
2023-07-07 14:39:19 -0300

    local

---
+++ local/bot.py
@@ -0,0 +1,38 @@
+import os
+import json
+import telebot
+import requests
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['analisar', 'Analisar'])
+def analisar(message):
+    ans = "Por gentileza, envie a foto que deseja analisar..."
+    sent_msg = bot.send_message(message.chat.id, ans, parse_mode="Markdown")
+    bot.register_next_step_handler(message, analisarFoto)
+
+@bot.message_handler(content_types=['photo'])
+def analisarFoto(message):
+    bot.send_message(message.chat.id, "Aguarde. Foto em análise...")
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+    image_headers = { 
+        'content-type' : 'image/jpeg'
+    }
+    r = requests.post("https://vmhjsq4lvxdkk7367d7eryoyjq0iutiy.lambda-url.us-east-1.on.aws/",headers=image_headers, data=downloaded_file)
+    try:
+        result = json.loads(r.text)
+        totalfaces = result['result']['totalfaces']
+        totalsmiling = result['result']['smiling']
+        msg = f"Total de Pessoas: {totalfaces}\nTotal Sorrindo: {totalsmiling}"
+        bot.send_message(message.chat.id, msg, parse_mode="Markdown")
+    except:
+        bot.send_message(message.chat.id, "Erro ao analisar a foto.", parse_mode="Markdown")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, "Função não reconhecida.")
+
+bot.infinity_polling()
---
+++ local/detectFace.py
@@ -0,0 +1,48 @@
+
+import cv2
+import sys
+
+def detectFace(image):
+  # Carregando os modelos
+  face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+  smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+  #Lendo a imagem
+  #img = cv2.imread(image)
+  img_np = cv2.imdecode(image, cv2.CV_LOAD_IMAGE_COLOR)
+
+  # Convertendo pra grayscale
+  gray = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+
+  # Detectando as faces
+  faces = face_cascade.detectMultiScale(gray, 1.1, 2)
+
+  # Detectando sorrisos
+  totalsmiles = 0
+  totalfaces = 0
+  for (x, y, w, h) in faces:
+      totalfaces = totalfaces + 1
+      cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = img[y:y+h, x:x+w]
+      smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 2)
+      if len(smiles) > 0:
+        totalsmiles = totalsmiles + 1
+        for (sx, sy, sw, sh) in smiles:
+           cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 1),
+
+  # Exibindo a Saida
+  print("Total Faces: " + str(totalfaces))
+  print("Total Sorrindo: " + str(totalsmiles))
+  cv2.imshow('img', img)
+  cv2.waitKey()
+  cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+  image_path = sys.argv[1]
+  with open(image_path, encoding="utf8", errors='ignore') as f:
+    #fd = open(image_path)
+    image = f.read()
+    #print(image)
+    f.close()
+    detectFace(image)
